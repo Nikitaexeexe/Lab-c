f0c0ee74f93db06ea4955ab300d9fbe5a1fd6d7b
Nikitaexeexe <125269070+Nikitaexeexe@users.noreply.github.com>
2023-04-06 20:42:06 +1000

    Add files via upload

---
+++ funcs.h
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdbool.h>
+
+double summ(double a, double b, double c) {
+    return (a + b + c);
+}
+
+double mult(double a, double b, double c) {
+    double p = summ(a, b, c) / 2;
+    return (sqrt(p*(p-a)*(p-b)*(p-c)));
+}
+bool res(double a, double b, double c) {
+ if ((a+b>c) && (a+c>b) && (b+c>a))
+  return true;
+ else
+  return false;
+ 
+}
+
